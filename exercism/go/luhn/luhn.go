d3e257efed3da77b0ac379b6de9156b98b549adc
mowzhja <mowzhja@protonmail.ch>
2021-09-09 12:56:04 +0200

    updated code

--- exercism/go/luhn/luhn.go
+++ exercism/go/luhn/luhn.go
@@ -1,33 +1,54 @@
 package luhn
 
 import (
-	sc "strconv"
-	s "strings"
+	"strconv"
+	"strings"
 )
 
 // Validates a number according to the Luhn algorithm (https://en.wikipedia.org/wiki/Luhn_algorithm)
 func Valid(number string) bool {
-	spacelessNumber := s.Replace(number, " ", "", -1)
+	spacelessNumber := strings.ReplaceAll(number, " ", "")
 	if !ok(spacelessNumber) {
 		return false
 	}
-	spacelessNumber = reverse(spacelessNumber) // makes the next step easier
-
 	doubledNumber := doubleDigits(spacelessNumber)
 
-	sumDigits := sum(doubledNumber)
-	return sumDigits%10 == 0
+	luhnSum := sumDigits(doubledNumber)
+	return luhnSum%10 == 0
 }
 
 // Creates the string with all the doubled digits.
-func doubleDigits(initNumber string) string {
-	var doubleSb s.Builder
+func doubleDigits(spacelessNumber string) string {
+	if len(spacelessNumber)%2 == 0 {
+		return evenLengthDouble(spacelessNumber)
+	}
+	return oddLengthDouble(spacelessNumber)
+}
 
-	for i, ch := range initNumber {
-		if i%2 != 0 {
-			doubleSb.WriteRune(double(ch))
+// Takes care of digit doubling if the length of the original string is even.
+func evenLengthDouble(spacelessNumber string) string {
+	var doubleSb strings.Builder
+
+	for i := 0; i < len(spacelessNumber); i++ {
+		if i%2 == 0 {
+			doubleSb.WriteByte(doubleChar(spacelessNumber[i]))
 		} else {
-			doubleSb.WriteRune(ch)
+			doubleSb.WriteByte(spacelessNumber[i])
+		}
+	}
+
+	return doubleSb.String()
+}
+
+// Takes care of digit doubling if the length of the original string is odd.
+func oddLengthDouble(spacelessNumber string) string {
+	var doubleSb strings.Builder
+
+	for i := 0; i < len(spacelessNumber); i++ {
+		if i%2 != 0 {
+			doubleSb.WriteByte(doubleChar(spacelessNumber[i]))
+		} else {
+			doubleSb.WriteByte(spacelessNumber[i])
 		}
 	}
 
@@ -35,7 +56,7 @@ func doubleDigits(initNumber string) string {
 }
 
 // Double a single character (rune), as long as that rune represents a digit.
-func double(ch rune) rune {
+func doubleChar(ch byte) byte {
 	n := int(ch - '0') // good ol' C-style trick
 
 	m := 2 * n
@@ -43,11 +64,11 @@ func double(ch rune) rune {
 		m -= 9
 	}
 
-	return rune(m + '0')
+	return byte(m + '0')
 }
 
 // Sums all the digits making up a string (supposedly a number).
-func sum(number string) int {
+func sumDigits(number string) int {
 	sum := 0
 
 	for _, ch := range number {
@@ -58,23 +79,12 @@ func sum(number string) int {
 	return sum
 }
 
-// Reverses a string char by char.
-func reverse(str string) string {
-	var rev s.Builder
-
-	for i := len(str) - 1; i >= 0; i-- {
-		rev.WriteByte(str[i])
-	}
-
-	return rev.String()
-}
-
 // Checks if the number string received in input is valid (aka has length greater than 1 and is only made up of digits).
 func ok(number string) bool {
 	if len(number) <= 1 {
 		return false
 	}
-	if _, err := sc.ParseInt(number, 10, 64); err != nil {
+	if _, err := strconv.ParseInt(number, 10, 64); err != nil {
 		return false
 	}
 
