6f3e410fe6e6f31f3e3b7a1e9650455f8158a3ca
mowzhja <mowzhja@protonmail.ch>
2021-09-18 12:33:55 +0200

    added some code to the solution

--- exercism/go/simple-cipher/simple_cipher.go
+++ exercism/go/simple-cipher/simple_cipher.go
@@ -8,24 +8,44 @@ type Caesar struct {
 	shift int32
 }
 
-func NewCaesar() Caesar {
-	return Caesar{shift: 5}
+func NewCaesar() Cipher {
+	return Caesar{shift: 3} // value used in tests
 }
 
-func (c *Caesar) Encode(pt string) string {
+func (c Caesar) Encode(pt string) string {
 	var ct strings.Builder
 
 	clean_pt := clean_input(pt)
 	for _, r := range clean_pt {
-		delta := ((r + c.shift) % 97) % 26 // 'a' == 97
+		delta := ((r - 'a') + c.shift) % 26
 		ct.WriteRune('a' + delta)
 	}
 
 	return ct.String()
 }
 
-func (c *Caesar) Decode(ct string) string {
+func (c Caesar) Decode(ct string) string {
+	var pt strings.Builder
+
 	clean_ct := clean_input(ct)
+	for _, r := range clean_ct {
+		delta := ((r - 'a') - c.shift) % 26
+		pt.WriteRune('a' + delta)
+	}
+
+	return pt.String()
+}
+
+func NewShift(new_shift int) Cipher {
+	if new_shift <= -26 || new_shift == 0 || new_shift >= 26 {
+		return nil
+	}
+
+	return Caesar{shift: int32(new_shift)}
+}
+
+func NewVigenere(key string) Cipher {
+	return nil
 }
 
 // Transforms the input string into a lowercase one with no punctuation.
