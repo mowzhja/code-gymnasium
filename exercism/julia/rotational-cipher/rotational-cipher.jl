cc27630669a0c1aa23e311c0d7417dd304a76c80
mowzhja <mowzhja@protonmail.ch>
2021-09-22 18:02:16 +0200

    refactored the code

--- exercism/julia/rotational-cipher/rotational-cipher.jl
+++ exercism/julia/rotational-cipher/rotational-cipher.jl
@@ -1,19 +1,15 @@
-"""Rotates the character `plainchar` by an amount=`amount`."""
-function rotate(amount, plainchar::Char)
+"""Rotates the character `plainchar` by an amount=`n`."""
+function rotate(n, plainchar::Char)
     if !isletter(plainchar)
         return plainchar
     end
 
-    shifted = plainchar + amount
-
     if isuppercase(plainchar)
-        return getindex('A':'Z', (shifted - 'A') % 26 + 1)
+        return getindex('A':'Z', (plainchar + n - 'A') % 26 + 1)
     end
 
-    return getindex('a':'z', (shifted - 'a') % 26 + 1)
+    return getindex('a':'z', (plainchar + n - 'a') % 26 + 1)
 end
 
-"""Rotates the string `plaintext` by an amount=`amount`."""
-rotate(amount, plaintext::String) = join(
-  rotate(amount, character) for character in plaintext
-  )
+"""Rotates the string `plaintext` by an amount=`n`."""
+rotate(n, plaintext::String) = map(char -> rotate(n, char), plaintext) 
